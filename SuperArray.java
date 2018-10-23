baef3baeb276c1c2e969a14ac6c54b9734a74713
vlu00 <vlu00@stuy.edu>
2018-10-23 10:11:10 -0400

    classwork

---
+++ SuperArray.java
@@ -0,0 +1,40 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    data = new String[10];
+    size = 0;
+  }
+
+  public void clear() {
+    size = 0;
+    //needs to remove all elements
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return (size == 0);
+  }
+
+  public boolean add(String) {
+    data = 
+  }
+  /*public String toString() {
+    return "[ ";
+    for (int i = 0; i < size; i++) {
+      return data[i];
+    }
+    return " ]";
+  }
+  */
+}
+
+
+
+
+
+}
